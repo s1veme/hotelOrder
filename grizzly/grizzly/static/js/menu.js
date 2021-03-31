a8fd7d837aeea5bbf88acaff3396d5b6f2591dcc
oldman <dudashvilivladislav@mail.ru>
2021-03-31 21:45:47 +0300

    Сжал HTML, JS, CSS файлы

--- grizzly/grizzly/static/js/menu.js
+++ grizzly/grizzly/static/js/menu.js
@@ -1,11 +1,1 @@
-function openFunction() {
-    document.getElementById("menu").style.width = "300px";
-    document.getElementById("menubox").style.marginLeft = "300px";
-    document.getElementById("menubox").innerHTML = "";
-}
-
-function closeFunction() {
-    document.getElementById("menu").style.width = "0px";
-    document.getElementById("menubox").style.marginLeft = "0px";
-    document.getElementById("menubox").innerHTML = "&#9776;";
-}
+function openFunction(){document.getElementById("menu").style.width="300px",document.getElementById("menubox").style.marginLeft="300px",document.getElementById("menubox").innerHTML=""}function closeFunction(){document.getElementById("menu").style.width="0px",document.getElementById("menubox").style.marginLeft="0px",document.getElementById("menubox").innerHTML="&#9776;"}
